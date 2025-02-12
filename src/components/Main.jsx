f59b5d9bd2dcbe644ea17d4b05a5641f18a71cdd
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-12 17:06:34 +0100

    create main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,28 @@ export default function Main() {
             <div className="container_Top">
                 <h1>-- Content goes here --</h1>
             </div>
+            <div className="container_down">
+                <div className="card">
+                    <img src="../../buy-comics-digital-comics.png" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                </div>
+                <div className="card">
+                    <img src="../../buy-comics-merchandise.png" alt="" />
+                    <a href="">DC MERCHANDISE</a>
+                </div>
+                <div className="card">
+                    <img src="../../buy-comics-shop-locator.png" alt="" />
+                    <a href="">SUBSCRIPTION</a>
+                </div>
+                <div className="card">
+                    <img src="../../buy-comics-subscriptions.png" alt="" />
+                    <a href="">COMIC SHOP LOCATOR</a>
+                </div>
+                <div className="card">
+                    <img src="../../buy-dc-power-visa.svg" alt="" />
+                    <a href="">DC POWER VISA</a>
+                </div>
+            </div>
 
         </main>
     )
