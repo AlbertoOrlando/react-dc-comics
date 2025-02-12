a6121d1e6ef3cd1ed1c644c1d8f161b773b74a57
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-12 16:30:56 +0100

    creat header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="container_Header">
+                <div><img src="../../public/dc-logo.png" alt="" /></div>
+                <div className="lista_Header">
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <div className="container_Top">
+                <h1>-- Content goes here --</h1>
+            </div>
+
+        </main>
+    )
+}
