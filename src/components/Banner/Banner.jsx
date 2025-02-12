4289f112868826befb118c263b5c0ecaef735ff9
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-12 18:52:03 +0100

    bonus create new component banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+export default function Banner() {
+    return (
+        <div className="container_down">
+            <div className="card">
+                <img src="../buy-comics-digital-comics.png" alt="" />
+                <a href="">DIGITAL COMICS</a>
+            </div>
+            <div className="card">
+                <img src="../buy-comics-merchandise.png" alt="" />
+                <a href="">DC MERCHANDISE</a>
+            </div>
+            <div className="card">
+                <img src="../buy-comics-shop-locator.png" alt="" />
+                <a href="">SUBSCRIPTION</a>
+            </div>
+            <div className="card">
+                <img src="../buy-comics-subscriptions.png" alt="" />
+                <a href="">COMIC SHOP LOCATOR</a>
+            </div>
+            <div className="card">
+                <img src="../buy-dc-power-visa.svg" alt="" />
+                <a href="">DC POWER VISA</a>
+            </div>
+        </div>
+    )
+}
